f6c02eed35c031d2f8469b73f3b27e7c046e37a2
sergeishmakov <m.shmakov.sergei@yandex.ru>
2019-08-05 18:15:03 +0300

    Added reducers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getTeamsAction } from './actions/teamsActions';
+import { getUsersAction } from './actions/usersActions';
 
 class App extends Component {
+  componentWillMount() {
+    this.props.getTeamsAction();
+    this.props.getUsersAction();
+  }
   render() {
     return this.props.children;
   }
 }
-export default App;
+
+const mapDispatchToProps = {
+  getTeamsAction,
+  getUsersAction
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import App from './App';
-import { Home } from './components/Home';
+import Teams from './components/Teams';
+import { Tasks } from './components/Tasks';
+import { Help } from './components/Help';
 import { NotFound } from './components/NotFound';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </App>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Switch>
+          <Redirect exact from='/' to='/teams' />
+          <Route path='/teams' component={Teams} />
+          <Route path='/tasks' component={Tasks} />
+          <Route path='/help' component={Help} />
+          <Route component={NotFound} />
+        </Switch>
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
